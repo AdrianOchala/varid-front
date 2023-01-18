650328f927e046315f789c56b2c88ccfdfd96597
AdrianOchala <ochala.adrian@gmail.com>
2023-01-18 19:59:09 +0100

    List of dogs + item component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-card
+        class="mx-2 my-12"
+        max-width="300"
+    >
+        <v-img 
+            height="250"
+            :src="item.photoLink"
+        ></v-img>
+        <v-card-title class="pb-0">{{ item.name }}</v-card-title>
+        <v-card-text>
+            <div class="pt-0 text-subtitle-1">
+                {{ item.breed }}
+            </div>
+            <v-divider class="mx-4"></v-divider>
+            <div class="mt-2">{{ item.description }}</div>
+        </v-card-text>
+        <v-card-actions class="d-flex justify-center">
+            <v-btn
+                color="blue"
+                outlined
+                @click="editDog"
+            >
+                Edit
+            </v-btn>
+            <v-btn
+                color="red"
+                outlined
+                @click="deleteDog"
+            >
+                Delete
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'ListItem',
+    props:{
+        item: {
+            type: Object,
+            default: null
+        },
+        index: {
+            type: Number,
+            default: null
+        }
+    },
+    methods: {
+        deleteDog() {
+            console.log("Deleting dog of index: " + this.index)
+        },
+        editDog() {
+            console.log("Editing dog of index: " + this.index)
+        },
+    }
+}
+</script>
+
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,6 +1,16 @@
 <template>
 	<v-container>
-		<h2 class='d-flex justify-center'>List of dogs</h2>
+		<h1 class='d-flex justify-center'>List of dogs</h1>
+        <div class="d-flex justify-center flex-wrap" v-if="dogs.length > 0">
+            <list-item v-for="(dog, index) in dogs" 
+                :key="'dog-'+index" 
+                :item="dog" 
+                :index="index"
+            ></list-item>
+        </div>
+        <div class="d-flex justify-center flex-wrap" v-else>
+            <span>Animal sheler is empty</span>
+        </div>
 	</v-container>
 </template>
 
@@ -8,13 +18,13 @@
 import { mapGetters } from 'vuex';
 export default {
 	name: 'ListView',
+    components:{
+        listItem: () => import('@/components/ListItem.vue')
+    },
     computed: {
         ...mapGetters({
             dogs: 'dogs/getDogs'
         })
-    },
-    created() {
-        console.log("Created ListView Dogs:", this.dogs)
     }
 }
 </script>
